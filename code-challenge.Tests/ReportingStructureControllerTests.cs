cc7c4d53fe80cef0018f2ba1e7fa8eb902846282
Joey Territo <jtt9340@rit.edu>
2021-12-20 21:34:50 -0500

    Create ReportingStructureController unit tests

--- code-challenge.Tests/ReportingStructureControllerTests.cs
+++ code-challenge.Tests/ReportingStructureControllerTests.cs
@@ -1,10 +1,68 @@
+using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 
 namespace code_challenge.Tests.Integration
 {
     [TestClass]
     public class ReportingStructureControllerTests
     {
-        
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetReportingStructureByEmployeeId_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedFirstName = "John";
+            var expectedLastName = "Lennon";
+            var expectedPosition = "Development Manager";
+            var expectedDepartment = "Engineering";
+            var expectedDirectReports = new[]
+            {
+                "b7839309-3348-463b-a7e3-5de1c168beb3",
+                "03aa1462-ffa9-4978-901b-7c001562cf6f"
+            };
+            var expectedNumberOfReports = 4;
+            
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reportingStructure/{employeeId}");
+            var response = getRequestTask.Result;
+            
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var reportingStructure = response.DeserializeContent<ReportingStructure>();
+            Assert.AreEqual(expectedFirstName, reportingStructure.Employee.FirstName);
+            Assert.AreEqual(expectedLastName, reportingStructure.Employee.LastName);
+            Assert.AreEqual(expectedPosition, reportingStructure.Employee.Position);
+            Assert.AreEqual(expectedDepartment, reportingStructure.Employee.Department);
+            CollectionAssert.AreEquivalent(expectedDirectReports, reportingStructure.Employee.DirectReports?.Select(e => e.EmployeeId).ToArray());
+            Assert.AreEqual(expectedNumberOfReports, reportingStructure.NumberOfReports);
+        }
     }
 }
